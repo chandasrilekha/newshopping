d323a2c58cc9c0c6e9433684b8ed7c4cc6e36bfd
Srilekha Chanda <srilekha.chanda@gspann.com>
2023-09-28 22:40:13 +0530

    contact about page

--- src/app/pages/menupage/menupage.component.ts
+++ src/app/pages/menupage/menupage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-menupage',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./menupage.component.css']
 })
 export class MenupageComponent {
+
+  constructor(private param: ActivatedRoute, private service: OrderService) { }
+  getMenuId: any;
+  menuData: any;
+  ngOnInit(): void {
+    this.getMenuId = this.param.snapshot.paramMap.get('id');
+    console.log(this.getMenuId, 'getmenu');
+    if (this.getMenuId) 
+    {
+      this.menuData = this.service.foodDetails.filter((value) => {
+        return value.id == this.getMenuId;
+      });
+      console.log(this.menuData,'menudata>>');
+    }
+  }
 
 }
